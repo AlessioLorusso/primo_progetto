b695ae48cb718d73122ccd6d530796bb08a953d0
AlessioLorusso <alessio.lorusso@alessandrinimainardi.edu.it>
2023-10-05 13:06:33 +0200

    aggiornato parametro

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u@9b1d4scco74c1!+6!q-uknm$r624)etxidt06qirrtzi5v8m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
