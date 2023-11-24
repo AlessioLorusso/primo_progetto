3a5a10fd387219bcd689b7d78c90409fdf212ffb
AlessioLorusso <alessio.lorusso@alessandrinimainardi.edu.it>
2023-11-24 10:56:35 +0100

    inizio punto 19

--- news/views.py
+++ news/views.py
@@ -53,14 +53,14 @@ def articoloDetailView(request, pk):
     articolo = get_object_or_404(Articolo, pk=pk)
     context= {"articolo": articolo}
     return render(request, "articolo_detail.html", context)
-
+"""
 def listaArticoli(request, pk):
     articoli= Articolo.objects.filter(giornalista_id=pk)
     context= {
         'articoli': articoli,
     }
-    return render(request,'lista_articoli.html',context)
-
+    return render(request,'lista_articoli.html',context)"""
+    
 def listaArticoli(request, pk=None):
     if(pk==None):
         articoli= Articolo.objects.all()
@@ -80,12 +80,12 @@ def listaGiornalisti(request, pk):
 
 def queryBase(request):
     #1 tutti gli articoli scritti da giornalisti di un certo cognome:
-    articoli_cognome = Articolo.objects.filter(giornalista__cognome='Rossi')
+    articoli_cognome = Articolo.objects.filter(giornalista__cognome='Caressa')
     #2 totale
     numero_totali_articoli= Articolo.objects.count()
 
     #3 contare il numero di articoli scritti da un giornalista specifico:
-    giornalista_1= Giornalista.objects.get(id=1)
+    giornalista_1= Giornalista.objects.get(id=5)
     numero_articoli_giornalista_1 = Articolo.objects.filter(giornalista=giornalista_1).count()
 
     #4 ordinare gli articoli per numero di visualizzazioni in ordine decrescente 
@@ -95,13 +95,13 @@ def queryBase(request):
     articoli_senza_visualizzazioni = Articolo.objects.filter(visualizzazioni=0)
 
     #6 articolo più visualizzato
-    articolo_piu_visualizzato = Articolo.objects.order_by('visualizzazioni').first()
+    articolo_piu_visualizzato = Articolo.objects.order_by('-visualizzazioni').first()
 
     #7 tutti i giornalisti nati dopo una certa data
-    giornalisti_data = Giornalista.objects.filter(anno_di_nascita__gt=datetime.date(1900,1,1))
+    giornalisti_data = Giornalista.objects.filter(anno_di_nascita__gt=datetime.date(1990,1,1))
 
     #8 tutti gli articoli pubblicati in una data specifica
-    articoli_del_giorno= Articolo.objects.filter(dara=datetime.date(2023,1,1))
+    articoli_del_giorno= Articolo.objects.filter(data=datetime.date(2023,1,1))
 
     #9 tutti gli articoli pubblicati in un intervallo di date
     articoli_periodo = Articolo.objects.filter(data__range=(datetime.date(2023,1,1), datetime.date(2023,12,31)))
@@ -111,10 +111,10 @@ def queryBase(request):
     articoli_giornalisti = Articolo.objects.filter(giornalista__in=giornalisti_nati)
 
     #11 il giornalista più giovane
-    giornalista_giovane= Giornalista.objects.order_by('anno_di_nascita').first()
+    giornalista_giovane= Giornalista.objects.order_by('-anno_di_nascita').first()
 
     #12 il giornalista più anziano
-    giornalista_anziano= Giornalista.objects.order_by('-anno_di_nascita').first()
+    giornalista_anziano= Giornalista.objects.order_by('anno_di_nascita').first()
 
     #13 gli ultimi 5 articoli pubblicati
     ultimi= Articolo.objects.order_by('-data')[:5]
@@ -123,11 +123,17 @@ def queryBase(request):
     articoli_minime_visualizzazioni= Articolo.objects.filter(visualizzazioni__gte=100)
 
     #15 tutti gli articoli che contengono una certa parola nel titolo
-    articoli_parola = Articolo.objects.filter(titolo__incontais='importante')
+    articoli_parola = Articolo.objects.filter(titolo__icontains='ha')
+
+    #16 articoli pubblicati in un certo mese specifico:
+    # nota per poter modificare la data di un articolo togliere la proprietà auto_now=True al field data nel model
+    # poi dare i comandi makemigrations e migrate per applicare le modifiche al database
+    articoli_mese_anno= Articolo.objects.filter(data__month=1, data__year=2023) 
+
     #creare il dizionario context
     context = {
         'articoli_cognome':articoli_cognome,
-        'numero_totali_articoli': numero_totali_articoli,
+        'numero_totale_articoli': numero_totali_articoli,
         'giornalista_1': giornalista_1,
         'numero_articoli_giornalista_1': numero_articoli_giornalista_1,
         'articoli_ordinati': articoli_ordinati,
