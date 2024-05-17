dc7df4f56fc446b0094ba1005bb7e7946c47c1eb
AlessioLorusso <alessio.lorusso@alessandrinimainardi.edu.it>
2024-05-17 08:41:27 +0200

    aggiornato

--- forms_app/urls.py
+++ forms_app/urls.py
@@ -6,6 +6,6 @@ app_name = 'forms_app'
 urlpatterns = [
     path('contattaci/',contatti, name='contatti'),
     path('lista_contatti/',lista_contatti, name='lista_contatti'),
-    path('elimina_contatti/',elimina_contatti, name='elimina_contatti'),
+    path('elimina_contatti/',elimina_contatto, name='elimina_contatti'),
 
 ]
